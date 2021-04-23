e8d13caf2ed742da8427be01c46e80a29001c5d8
artem <artemufimtsev01@mail.ru>
2021-04-23 21:32:46 +0400

    Revert "Первая версия"
    This reverts commit 82298df5

---
+++ src/main/java/Student.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Student {
+    private static final List<String> SEPARATORS = Arrays.asList(" ", ",", ".", "!", "?", "\"", ";", ":", "[", "]", "(", ")", "\\r", "\\n", "\\t");
+
+    public Student() {
+        List<String> proba = new ArrayList<>();
+        List<String> proba1 = new LinkedList<>();
+    }
+}
